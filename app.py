469b6842b8aebd4619706fd4aaefc045a28ef27c
spiriton <i.ershov@innopolis.university>
2026-07-08 10:37:29 +0300

    улучшили прогноз погоды

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index2.html")
+    return render_template("index.html")
 
 @app.route('/forecast')
 def forecast():
-    return render_template("forecast.html")
+    return render_template("forecast.html", forecast="")
+
+@app.route('/get_forecast', methods=['POST'])
+def get_forecast():
+    city = request.form['city']
+    forecast = f"Прогноз погоды для {city}"
+    return render_template("forecast.html", forecast=forecast)
 
 app.run(debug=True)
